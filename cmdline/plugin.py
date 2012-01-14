77e9856f059ef16bea28f2b373d53f7197a190bc
Sven (Sbte) <svenb.linux@gmail.com>
2012-01-14 20:32:41 +0100

    cmdline: Restructure the cmdline plugin a bit

--- cmdline/plugin.py
+++ cmdline/plugin.py
@@ -23,114 +23,97 @@ import gui
 import subprocess
 import urllib2
 try:
-    import tinyurl  #you need to install tinyurl "easy_install tinyurl" before starting this plugin
+    import tinyurl  #you need to install tinyurl for using /tiny
 except ImportError:
     pass
 
 
 class Plugin(PluginBase):
     _description = 'Run some commands from window chat'
-    _authors = { 'James Axl and Sbte' : 'axlrose112@gmail.com' }
+    _authors = { 'James Axl' : 'axlrose112@gmail.com' , 'Sbte' : '' }
 
     def __init__(self):
         PluginBase.__init__(self)
-          
-    def start(self, session):    
+
+    def start(self, session):
         self.session = session
         self.session.signals.conv_started.subscribe(self.open_conv)
         self.soundPlayer = extension.get_and_instantiate('sound', session)
         self.homedir = os.path.expanduser('~/')
         return True
-       
+
     def stop(self):
         self.session.signals.conv_started.unsubscribe(self.open_conv)
         return False
 
-    def config(self, session):   
+    def config(self, session):
         pass
 
     def open_conv(self, cid, account):
         conversation = self.session.get_conversation(cid)
         conversation.input.on_send_message = Fun(self.custom_on_send_message, conversation)
-     
-               
-    def custom_on_send_message(self,conversation,text):
-        cid=conversation.cid
-        if text[0]=="/":
-            
-            cmds=["/help","/clear","/nudge","/run","/tiny"]
-            chck=text.split('[')
-            text=text.split(' ')
-            if text[0] in cmds:
-                if text[0]=="/help":
-                    help="""<ul><strong><li>/clear for cleaning chat window.</li></strong>
+
+    def custom_on_send_message(self, conversation, text):
+        cid = conversation.cid
+        if text.startswith("/"):
+            commands = ["/help", "/clear", "/nudge", "/run", "/tiny", "/all"]
+            chck = text.split('[')
+            text = text.split(' ')
+            command = text[0]
+            message = e3.Message(e3.Message.TYPE_INFO, '', None, None)
+            if command in commands:
+                if command == "/help":
+                    help = """<ul><strong><li>/clear for cleaning chat window.</li></strong>
                             <strong><li>/nudge for sending nudge to user.</li></strong>
                             <strong><li>/run for running command e.g /run mplayer [fatman.mp3], /run firefox or /run mplayer [james axl.flv]
                             <span>DEFAULT PATH IS YOUR HOME DIRECTORY</span>.</li></strong>
                             <strong><li>/tiny for sending large URL e.g /tiny large_url.</li></strong<ul>"""
-                    message=e3.Message(e3.Message.TYPE_INFO, help,'',timestamp=None)
-                    self.session.gui_message(cid,'',message)
-                elif text[0]=="/clear":
+                    message = e3.Message(e3.Message.TYPE_INFO, help, '', timestamp = None)
+                elif command == "/clear":
                     conversation.output.clear()
-                elif text[0]=="/nudge":
+                elif command == "/nudge":
                     self.session.request_attention(cid)
-                    message = e3.Message(e3.Message.TYPE_INFO, '', None, None)
                     message.body = _('You just sent a nudge!')
-                    self.session.gui_message(cid,'',message)
                     self.soundPlayer.play(gui.theme.sound_theme.sound_nudge)
-                elif text[0]=="/run":                
-                    if len(chck)==2:
+                elif command == "/run":
+                    if len(chck) == 2:
                         try:
-                            c=' '.join(text[1:])
-                            text=c.split('[')
-                            subprocess.Popen([text[0][:-1],self.homedir+text[1][:-1]])
+                            c = ' '.join(text[1:])
+                            text = c.split('[')
+                            subprocess.Popen([command[:-1], self.homedir+text[1][:-1]])
                         except NameError:
-                            message = e3.Message(e3.Message.TYPE_INFO, '', None, None)
-                            message.body="""<B>Command not found</B>"""
-                            self.session.gui_message(cid,'',message)
+                            message.body = "<b>Command not found</b>"
                         except OSError:
-                            message = e3.Message(e3.Message.TYPE_INFO, '', None, None)
-                            message.body="""<B>File not found</B>"""
-                            self.session.gui_message(cid,'',message)    
-                    elif len(text)==2:
+                            message.body = "<b>File not found</b>"
+                    elif len(text) == 2:
                         try:
                             subprocess.Popen([text[1]])
                         except OSError:
-                            message = e3.Message(e3.Message.TYPE_INFO, '', None, None) 
-                            message.body="""<B>Command not found</B>"""
-                            self.session.gui_message(cid,'',message)
-                    elif len(text)==1:
-                             message = e3.Message(e3.Message.TYPE_INFO, '', None, None) 
-                             message.body="""<B>Please type /help for more help</B>"""
-                             self.session.gui_message(cid,'',message)
-                elif text[0]=="/tiny":
-                    if len(text)!=2:
-                        message = e3.Message(e3.Message.TYPE_INFO, '', None, None)
-                        message.body="""<B>Please type /help for more help</B>"""
-                        self.session.gui_message(cid,'',message)
+                            message.body = "<b>Command not found</b>"
+                    elif len(text) == 1:
+                             message.body = "<b>Please type /help for more help</b>"
+                elif command == "/tiny":
+                    if len(text) != 2:
+                        message.body = "<b>Please type /help for more help</b>"
                     else:
                         req = urllib2.Request(text[1])
-                        try: 
+                        try:
                             urllib2.urlopen(req)
-                            url=tinyurl.create_one(text[1])
-                            message = e3.Message(e3.Message.TYPE_MESSAGE,url,None, None) #It is look ilogical but i need it :).
-                            self.session.gui_message(cid,'',message)
-                            message = e3.Message(e3.Message.TYPE_INFO, '', None, None)
-                            message.body="""<B><a href="%s">%s</a> was succeful generated and sent :)</B>"""%(url,url)
-                            self.session.gui_message(cid,'',message)
+                            url = tinyurl.create_one(text[1])
+                            message = e3.Message(e3.Message.TYPE_MESSAGE, url, None, None) #It is look ilogical but i need it :).
+                            self.session.gui_message(cid, '', message)
+                            message.type = e3.Message.TYPE_INFO
+                            message.body = "<b><a href = \"%s\">%s</a> was successfully generated and sent :)</b>" % (url, url)
                         except NameError:
-                            message = e3.Message(e3.Message.TYPE_INFO, '', None, None)
-                            message.body="""<b>Please install tinyurl, easy_install tinyurl</b>"""
-                            self.session.gui_message(cid,'',message)
+                            message.body = "<b>Please install tinyurl</b>"
                         except ValueError:
-                            message = e3.Message(e3.Message.TYPE_INFO, '', None, None)
-                            message.body="""<b>Check your URL</b>"""
-                            self.session.gui_message(cid,'',message)
-            
+                            message.body = "<b>Check your URL</b>"
+
             else:
-                message = e3.Message(e3.Message.TYPE_INFO, '', None, None)
-                message.body = """<B>Please type /help for more help</B>"""
-                self.session.gui_message(cid,'',message)
+                message.body = "<b>Please type /help for more help</b>"
+
+            if message.body:
+                self.session.gui_message(cid, '', message)
         else:
             conversation._on_send_message(text)
 
@@ -138,6 +121,6 @@ class Fun(object):                  #Sbte who has the idea for using this class 
     def __init__(self, fun, conv):
         self.fun = fun
         self.conv = conv
- 
+
     def __call__(self, text):
         self.fun(self.conv, text)
